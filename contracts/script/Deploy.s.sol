4b72b0957cf14d4d0c603e7b700ebc3d307a9acb
SendouD <janan.karur@gmail.com>
2025-04-15 19:06:10 +0530

    selvams commit transaction history

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {CookieJarRegistry} from "../src/CookieJarRegistry.sol";
+import {CookieJarFactory} from "../src/CookieJarFactory.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mock is ERC721 {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
+
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+}
+
+contract Deploy is Script {
+    HelperConfig helperConfig;
+    HelperConfig.NetworkConfig config;
+
+    function run() public {
+        helperConfig = new HelperConfig();
+        config = helperConfig.getAnvilConfig();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy Registry
+        CookieJarRegistry registry = new CookieJarRegistry();
+        ERC20Mock testtoken = new ERC20Mock();
+        testtoken.mint(deployer, 100e18);
+        console.log("CookieJarRegistry deployed at: ", address(registry));
+        console.log("Test ERC", address(testtoken));
+
+        // Deploy Factory with Registry address
+        CookieJarFactory factory = new CookieJarFactory(
+            config.defaultFeeCollector,
+            address(registry),
+            0x487a30c88900098b765d76285c205c7c47582512,
+            config.feePercentageOnDeposit,
+            config.minETHDeposit,
+            config.minERC20Deposit
+        );
+
+        console.log("CookieJarFactory deployed at: ", address(factory));
+
+        // Set Factory in Registry
+        registry.setCookieJarFactory(address(factory));
+
+        ERC721Mock erc721 = new ERC721Mock("TestNFT", "TNFT");
+        erc721.mint(deployer, 1);
+        console.log("ERC721Mock deployed at:", address(erc721));
+        console.log("Token ID 1 minted to:", deployer);
+
+        vm.stopBroadcast();
+    }
+}
