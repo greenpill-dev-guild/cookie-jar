dd911c0703ff647c9256c1e74fd7ad96242df319
michaelgreen06 <michaelgreen06@gmail.com>
2025-05-19 16:31:16 -0600

    removed profile from sidebar

--- frontend/components/design/collapsible-sidebar.tsx
+++ frontend/components/design/collapsible-sidebar.tsx
@@ -109,7 +109,8 @@ export function CollapsibleSidebar() {
         <div className="mt-auto px-4 pt-4 space-y-2">
           {isConnected && (
             <>
-              <Link
+            {/* removed profile view for now since it didn't work properly <3MSG 5/19/25 */}
+              {/* <Link
                 href="/profile"
                 className={cn(
                   "flex items-center h-14 px-4 rounded-xl transition-all duration-200",
@@ -139,7 +140,7 @@ export function CollapsibleSidebar() {
                     Profile
                   </div>
                 </div>
-              </Link>
+              </Link> */}
 
               <button
                 onClick={() => disconnect()}
